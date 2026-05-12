ff3f77ef83f296dd9f8f0dbc5cf72a63c8dca41b
taxukarat <karatzidis.taxiarchis@gmail.com>
2026-05-12 22:24:11 +0300

    [is_alpha] Made a function that checks if a character is alphabetic and returns true or false.

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all:
         @nasm -f elf32 strlen.s -o strlen.o
+        @nasm -f elf32 is_alpha.s -o is_alpha.o
 clean:
-        rm strlen.o
+        rm strlen.o is_alpha.o
---
+++ is_alpha.s
@@ -0,0 +1,28 @@
+%define true 1
+%define false 0
+section .text
+global is_alpha
+
+is_alpha:
+        push ebp
+        mov ebp, esp
+        mov al, [esp+0x8]
+        cmp al, 122
+        ja err
+        cmp al, 90
+        jb cap
+        cmp al, 97
+        jb err
+        jmp correct
+cap:
+        cmp al, 65
+        jb err
+correct:
+        mov eax, 1
+is_alpha_ret:
+        mov esp, ebp
+        pop ebp
+        ret
+err:
+        mov eax, 0
+        jmp is_alpha_ret
